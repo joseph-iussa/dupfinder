ccf51f9f662916e225e64109af5dcf8fc9ceb9a3
Joseph Iussa <jiussa@internode.on.net>
2017-04-07 02:01:38 +0930

    Added function and test to get absolute file paths.

--- dupfinder/core.py
+++ dupfinder/core.py
@@ -1,8 +1,21 @@
+import os
+
 from collections import namedtuple
 from itertools import groupby
 
 
 FileHashInfo = namedtuple('FileHashInfo', ['hash', 'path'])
+
+
+def get_absolute_file_paths(root_paths):
+    abs_paths = set()
+
+    for root_path in root_paths:
+        for dirpath, dirnames, filenames in os.walk(root_path):
+            for name in filenames:
+                abs_paths.add(os.path.abspath(os.path.join(dirpath, name)))
+
+    return list(abs_paths)
 
 
 def get_file_bytes(path):
--- tests/integration/core_test.py
+++ tests/integration/core_test.py
@@ -1,1 +1,24 @@
-pass
+from pathlib import Path
+from dupfinder.core import get_absolute_file_paths
+
+
+def test__get_absolute_file_paths__gets_all_paths(tmpdir):
+    test_paths = (
+        Path(str(tmpdir), 'foo.txt'),
+        Path(str(tmpdir), 'sub/foo.txt'),
+        Path(str(tmpdir), 'sub/bar.txt'),
+        Path(str(tmpdir), 'sub/sub/baz.txt'),
+        Path(str(tmpdir), 'sub/sub/box.txt'),
+    )
+    for path in test_paths:
+        if not path.parent.exists():
+            path.parent.mkdir(parents=True)
+        path.touch()
+    test_root_paths = (
+        str(Path(str(tmpdir), '.').resolve()),
+        str(Path(str(tmpdir), 'sub').resolve())
+    )
+
+    returned_paths = get_absolute_file_paths(test_root_paths)
+
+    assert all(str(test_path.resolve()) in returned_paths for test_path in test_paths)
